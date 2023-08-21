31f63f91fb097f4a125d10d01585fe2070a70cf4
mikael <mikaeljeffeson@gmail.com>
2023-08-21 16:30:03 -0300

    fix lint1

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,8 +2,6 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 def test_encrypt_message():
-
-
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("xesq", "dele")
     with pytest.raises(TypeError, match="tipo inválido para message"):
