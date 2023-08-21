81c18fd4f6f98ffe260fa850725d3457dbf1a446
mikael <mikaeljeffeson@gmail.com>
2023-08-21 10:57:40 -0300

    Feat: Req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter_study = 0
+    if type(target_time) != int:
+        return None
+    for [period_start, period_end] in permanence_period:
+        if type(period_start) != int or type(period_end) != int:
+            return None
+        if period_start <= target_time and period_end >= target_time:
+            counter += 1
+    return counter_study
