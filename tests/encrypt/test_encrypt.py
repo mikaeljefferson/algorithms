1bf94eead22463c918df677a78f6e02949f40abb
mikael <mikaeljeffeson@gmail.com>
2023-08-21 16:15:24 -0300

    fix lint222

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,14 +1,15 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
-def test_encrypt_message():
+def  test_encrypt_message():
+
 
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message("xesq", "dele")
-  
+       encrypt_message("xesq", "dele")
+
     with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message(22, 666)
+       encrypt_message(22, 666)
 
     assert encrypt_message("teste", 10) == "etset"
     assert encrypt_message("teste", 3) == "set_et"
